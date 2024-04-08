7d73e6c6330dc4f8034cf2af468dbe414617ce5d
Cameron Williams <cswilliams112004@gmail.com>
2024-04-07 21:07:15 -0400

    trying to get the 3d model to load but it's not working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,21 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Cameron</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I'm a developer specializing in React and JavaScript for front-end
+            development, with experience in Python and Supabase for backend
+            services.
+          </p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   );
 };
