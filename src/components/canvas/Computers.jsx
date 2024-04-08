ae4f606cfeb85aad60c84b9f9dd2cbb0f22305ee
Cameron Williams <cswilliams112004@gmail.com>
2024-04-07 21:07:43 -0400

    trying to get the 3d model to load but it's not working

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+// import { Suspense, useEffect, useState } from "react";
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+// import CanvasLoader from "../Loader";
+
+// const Computers = () => {
+//   const computer = useGLTF("./desktop_pc/scene.gltf");
+
+//   return (
+//     <mesh>
+//       <hemisphereLight intensity={0.15} groundColor="black" />
+//       <pointLight intensity={1} />
+//       <primitive
+//         object={computer.scene}
+//         scale={0.75}
+//         position={[0, -3.25, -1.5]}
+//         rotation={[-0.01, -0.2, -0.1]}
+//       />
+//     </mesh>
+//   );
+// };
+
+// const ComputersCanvas = () => {
+//   return (
+//     <Canvas
+//       frameLoop="demand"
+//       shadows
+//       camera={{ position: [20, 3, 5], fov: 25 }}
+//       gl={{ preserveDrawingBuffer: true }}
+//     >
+//       <Suspense fallback={<CanvasLoader />}>
+//         <OrbitControls
+//           enableZoom={false}
+//           maxPolorAngle={Math.PI / 2}
+//           minPolorAngle={Math.PI / 2}
+//         />
+//         <Computers />
+//       </Suspense>
+
+//       <Preload all />
+//     </Canvas>
+//   );
+// };
+
+// export default Computers;
+
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const { scene } = useGLTF("./desktop_pc/scene.gltf");
 
-export default Computers
+  return (
+    <>
+      <ambientLight intensity={0.3} />
+      <HemisphereLight
+        intensity={0.15}
+        groundColor="black"
+        skyColor="#ffffff"
+      />
+      <PointLight intensity={1} position={[0, 5, 0]} />
+      <primitive
+        object={scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
