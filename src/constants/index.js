1434f9cceeea972764a1ca99e77d484f0a0e876c
Cameron Williams <cswilliams112004@gmail.com>
2024-04-10 19:47:42 -0400

    changed profile pic path

--- src/constants/index.js
+++ src/constants/index.js
@@ -176,7 +176,7 @@ const testimonials = [
     name: "James Figeuroa",
     designation: "Branch Manager",
     company: "Weed Man Lawn Care",
-    image: "public/Jamespfp.jpeg",
+    image: "src/assets/Jamespfp.png",
   },
   {
     testimonial:
@@ -184,7 +184,7 @@ const testimonials = [
     name: "John Wilson",
     designation: "General Manager",
     company: "Weed Man Lawn Care",
-    image: "public/Johnpfp.jpeg",
+    image: "src/assets/Johnpfp.png",
   },
 ];
 
@@ -208,48 +208,6 @@ const projects = [
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Job IT",
-    description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "restapi",
-        color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: jobit,
-    source_code_link: "https://github.com/",
-  },
-  {
-    name: "Trip Guide",
-    description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-    tags: [
-      {
-        name: "nextjs",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "supabase",
-        color: "green-text-gradient",
-      },
-      {
-        name: "css",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: tripguide,
     source_code_link: "https://github.com/",
   },
 ];
